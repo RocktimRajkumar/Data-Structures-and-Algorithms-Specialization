6addf72819968daabb7ed13fef8d2836f8498ad2
Rocktim <rajkumar.rocktim@gmail.com>
2019-04-28 22:20:59 +0530

    Solution : Course 1/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum

--- Course 1/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/FibonacciPartialSum.java
+++ Course 1/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/FibonacciPartialSum.java
@@ -19,12 +19,46 @@ public class FibonacciPartialSum {
 
         return sum % 10;
     }
+
+    private static long getFibonacciPartialSumFast(long from,long to){
+        if(to==0)
+        return 0;
+        long start=(from)%60;
+        long end=(to+1)%60;
+
+        long f1=0;
+        long f2=1;
+        long f=0;
+        long ff=1;
+        for(int i=0;i<start-2;i++){
+            ff=(f1+f2)%10;
+            f1=f2;
+            f2=ff;
+        }
+        if(start<2){
+            start=2;
+            f=1;
+        }
+        if(end==0)
+            return 0;
+        for(long i=start;i<end;i++){
+            
+           
+            long temp=f1;
+            f1=f2;
+            f2=(temp+f2)%10;
+            f=(f+f2)%10;
+        }
+
+        return f;
+    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+        // System.out.println(getFibonacciPartialSumNaive(from, to));
+        System.out.println(getFibonacciPartialSumFast(from, to));
     }
 }
 
