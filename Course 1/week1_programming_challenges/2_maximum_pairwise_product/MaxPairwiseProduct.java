a5f5c4e67a7e68420233d6fdfc47f98bdc468ba1
Rocktim <rajkumar.rocktim@gmail.com>
2019-04-28 22:06:00 +0530

    Solution of Course 1/week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct

--- Course 1/week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
+++ Course 1/week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
@@ -1,32 +1,74 @@
 import java.util.*;
 import java.io.*;
-
-public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers) {
-        int max_product = 0;
-        int n = numbers.length;
-
-        for (int first = 0; first < n; ++first) {
-            for (int second = first + 1; second < n; ++second) {
-                max_product = max(max_product,
-                    numbers[first] * numbers[second])
-            }
-        }
-
-        return max_product;
-    }
-
-    public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        System.out.println(getMaxPairwiseProduct(numbers));
-    }
-
-    static class FastScanner {
+class MaxPairwiseProduct{
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []v=new int[n];
+		for(int i=0;i<n;i++){
+			v[i]=sc.nextInt();
+		}
+		System.out.println(MaxPairwiseProduct(v));
+		//System.out.println(MaxPairwiseProductNaive(v));*/
+		// while(true){
+		// 	Random rand=new Random();
+		// 	int len=rand.nextInt(15050)+2;
+		// 	int []v=new int[len];
+		// 	for(int i=0;i<len;i++){
+		// 		v[i]=rand.nextInt(9000);
+		// 	}
+		
+		// 	for(int i=0;i<len;i++){
+		// 		System.out.print(v[i]+" ");
+		// 	}
+		// 	System.out.println();
+		// 	Long f=MaxPairwiseProduct(v);
+		// 	Long b=MaxPairwiseProductNaive(v);
+		// 	if(f.compareTo(b)!=0){
+		// 		System.out.println("wrong answer "+f+" "+b);
+		// 		break;
+		// 	}
+		// 	else{
+		// 		System.out.println("OK");
+		// 	}
+		// }
+	}
+	
+	
+	public static Long MaxPairwiseProductNaive(int[] v){
+		int len=v.length;
+		Long product=0L;
+		for(int i=0;i<len;i++){
+			for(int j=0;j<len;j++){
+				if(i!=j && (v[i]*v[j])>product){
+					product=(long)v[i]*(long)v[j];
+				}
+			}
+		}
+		return product;
+	}
+	
+	public static Long MaxPairwiseProduct(int[] v){
+		
+		int len=v.length;
+		int first_max=0;
+		int second_max=0;
+		for(int i=1;i<len;i++){
+			if(v[i]>v[first_max]){
+				first_max=i;
+			}
+		}
+		int temp=v[first_max];
+		v[first_max]=v[len-1];
+		v[len-1]=temp;
+		for(int i=1;i<len-1;i++){
+			if(v[i]>v[second_max]){
+				second_max=i;
+			}
+		}
+		return (long)v[len-1]*(long)v[second_max];
+	}
+	static class FastScanner {
         BufferedReader br;
         StringTokenizer st;
 
@@ -54,5 +96,4 @@ public class MaxPairwiseProduct {
             return Integer.parseInt(next());
         }
     }
-
 }
