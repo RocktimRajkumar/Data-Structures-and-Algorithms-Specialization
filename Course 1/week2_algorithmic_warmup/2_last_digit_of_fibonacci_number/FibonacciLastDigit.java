6bce8c4d7f96f31d3d2ff19b9a9fbeca842d01db
Rocktim <rajkumar.rocktim@gmail.com>
2019-04-28 22:11:33 +0530

    Solution : Course 1/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit

--- Course 1/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
+++ Course 1/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -16,12 +16,26 @@ public class FibonacciLastDigit {
 
         return current % 10;
     }
+	
+	private static int getFibonacciLastDigitFast(int n){
+		if(n<=1)
+			return n;
+		int []arr=new int[n];
+		arr[0]=0;
+		arr[1]=1;
+		for(int i=2;i<n;i++){
+			arr[i]=(arr[i-1]+arr[i-2])%10;
+		}
+		return (arr[n-1]+arr[n-2])%10;
+		
+	}
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        int c = getFibonacciLastDigitFast(n);
         System.out.println(c);
     }
 }
+
 
