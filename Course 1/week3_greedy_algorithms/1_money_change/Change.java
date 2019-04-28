258e90f0cbcb559ac2854ef8a45b6236dda620cc
Rocktim <rajkumar.rocktim@gmail.com>
2019-04-28 22:24:20 +0530

    Solution : Course 1/week3_greedy_algorithms/1_money_change/Change

--- Course 1/week3_greedy_algorithms/1_money_change/Change.java
+++ Course 1/week3_greedy_algorithms/1_money_change/Change.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Change {
     private static int getChange(int m) {
         //write your code here
-        return m;
+        int count=0;
+        while(m!=0){
+            if(m>=10)
+                m-=10;
+            else if(m>=5)
+                m-=5;
+            else
+                m-=1;
+            count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
